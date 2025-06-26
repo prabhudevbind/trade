d1d649141372b2589022499dee5a346068f2e5ce
prabhudevbind <devcoder2323@gmail.com>
2025-06-27 03:42:40 +0530

    jj

--- server/dist/assets/service-worker-BGlgtLKe-2025-06-26T22-00-54-037Z.js
+++ server/dist/assets/service-worker-BGlgtLKe-2025-06-26T22-00-54-037Z.js
@@ -1,2 +1,2 @@
 self.addEventListener("push",function(t){if(t.data){const i=t.data.json();if(i.type==="check")return;t.waitUntil(self.registration.showNotification(i.title||"New Message",{body:i.message,icon:"/notification-icon.png",badge:"/notification-badge.png",data:i}))}});
-//# sourceMappingURL=service-worker-BGlgtLKe-2025-06-26T21-45-43-848Z.js.map
+//# sourceMappingURL=service-worker-BGlgtLKe-2025-06-26T22-00-54-037Z.js.map
